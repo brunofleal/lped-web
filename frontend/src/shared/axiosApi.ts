0c2858cf99328c65ed5c0baf959141b066413a15
Bruno <brunofilizolaleal@gmail.com>
2023-10-13 21:49:04 -0300

    Load server url from backend

--- frontend/src/shared/axiosApi.ts
+++ frontend/src/shared/axiosApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8000';
+const BASE_URL = '/';// process.env.REACT_SERVER_URL;
 
 const axiosApi = axios.create({
     baseURL: BASE_URL,
