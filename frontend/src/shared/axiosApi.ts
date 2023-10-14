5b64c428688534ef79be9d7a8f883d2ffef0a011
Bruno <brunofilizolaleal@gmail.com>
2023-10-14 14:13:45 -0300

    adjust base url

--- frontend/src/shared/axiosApi.ts
+++ frontend/src/shared/axiosApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = '/';// process.env.REACT_SERVER_URL;
+const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : '/';
 
 const axiosApi = axios.create({
     baseURL: BASE_URL,
