3f950f24d1721ccc0d78061a8fe7e288d3979893
Bruno <brunofilizolaleal@gmail.com>
2023-12-31 19:13:53 -0300

    Change favicon/page name; start work on deploy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,28 @@
 import { ChakraBaseProvider } from '@chakra-ui/react';
 import chakraTheme from '@chakra-ui/theme';
 import React, { FC } from 'react';
+import { Helmet } from 'react-helmet';
 import {
     RouterProvider,
 } from 'react-router-dom';
 
+import favicon from './assets/favicon/favicon-16x16.png';
 import router from './shared/router';
 
 
 const App: FC = () => {
     return (
-        <ChakraBaseProvider theme={chakraTheme}>
-            <RouterProvider router={router} />
-        </ChakraBaseProvider>
+        <>
+            <Helmet>
+                <title>Mipe Alegre</title>
+                <meta name="description" content="Liga Mipe Alegre de dota 2" />
+                <meta name="theme-color" content="#008f68" />
+                <link rel="icon" type="image/png" href={favicon} sizes="16x16" />
+            </Helmet>
+            <ChakraBaseProvider theme={chakraTheme}>
+                <RouterProvider router={router} />
+            </ChakraBaseProvider>
+        </>
     );
 };
 
